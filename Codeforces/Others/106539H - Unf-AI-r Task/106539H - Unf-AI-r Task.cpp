996b368fad57dec7c3ce5350245e3634e5014d29
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-16 17:50:05 +0600

    [106539H - Unf-AI-r Task | Gym] Accepted | Time: 31ms | Memory: 0KB

---
+++ Codeforces/Others/106539H - Unf-AI-r Task/106539H - Unf-AI-r Task.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+
+
+
+
+void solve() {
+    cout << 178632754 << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    //cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+}
