408c8707677a8e34234187c4f50f9d24744c7997
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-16 17:56:56 +0600

    [2148A - Sublime Sequence | 800] Accepted | Time: 46ms | Memory: 0KB

---
+++ Codeforces/Div. 4/2148A - Sublime Sequence/2148A - Sublime Sequence.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    long long n,x;
+    cin>>x>>n;
+    if(n%2) cout<<x<<endl;
+    else cout<<"0"<<endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+        
+}
