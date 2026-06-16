ae018c3f3e40fd8bb93ec19e4b9e8b3148292f88
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-16 17:58:54 +0600

    [1692A - Marathon | 800] Accepted | Time: 78ms | Memory: 100KB

---
+++ Codeforces/Div. 4/1692A - Marathon/1692A - Marathon.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    long long a,b,c,d;
+    cin>>a>>b>>c>>d;
+    int count=0;
+    if(b>a) count++;
+    if(c>a) count++;
+    if(d>a) count++;
+    
+    cout<<count<<endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+        
+}
