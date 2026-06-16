f46fd6a9eaba40186b503ca8f2340eb921e59d8f
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-16 18:00:36 +0600

    [71A - Way Too Long Words | 800] Accepted | Time: 46ms | Memory: 100KB

---
+++ Codeforces/Div. 2/71A - Way Too Long Words/71A - Way Too Long Words.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin>>s;
+    if (s.length()>10)
+    {
+        cout<<s[0]<<s.length()-2<<s[s.length()-1]<<endl;
+    }
+    else cout<<s<<endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+}
