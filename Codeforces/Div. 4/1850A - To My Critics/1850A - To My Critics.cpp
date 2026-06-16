1c7b52fb3c33d161c9f21dbe81c728738b30adcc
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-16 17:58:42 +0600

    [1850A - To My Critics | 800] Accepted | Time: 31ms | Memory: 0KB

---
+++ Codeforces/Div. 4/1850A - To My Critics/1850A - To My Critics.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    long long a,b,c;
+    cin>>a>>b>>c;
+    if(a+b>=10 || a+c>=10 || b+c>=10) 
+    {
+        cout<<"YES"<<endl; return;
+    }
+    else cout<<"NO"<<endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+        
+}
