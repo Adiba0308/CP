a6ede24b7fb3395e7604ffa97c73e3382da61f0a
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-16 18:00:25 +0600

    [579A - Raising Bacteria | 1000] Accepted | Time: 46ms | Memory: 100KB

---
+++ Codeforces/Div. 2/579A - Raising Bacteria/579A - Raising Bacteria.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    unsigned long long n;
+    cin>>n;
+    cout<<popcount(n)<<endl;
+}
+
+int main()
+{
+    solve();
+        
+}
