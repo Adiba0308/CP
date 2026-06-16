047fb07b6c1093c3b90a5a67c36d1a62dc0c440f
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-16 17:56:02 +0600

    [486A - Calculating Function | 800] Accepted | Time: 46ms | Memory: 100KB

---
+++ Codeforces/Div. 2/486A - Calculating Function/486A - Calculating Function.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    long long n,f=0;
+    cin>>n;
+    if(n%2) cout<<(n+1)/2*-1<<endl;
+    else cout<<n/2<<endl;
+}
+
+int main()
+{
+    solve();
+        
+}
