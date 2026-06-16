c3cffe5df094ce94792c14c690e7eadb64eb8932
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-16 17:55:46 +0600

    [2214D - Neural Feud | Gym] Accepted | Time: 31ms | Memory: 100KB

---
+++ Codeforces/Others/2214D - Neural Feud/2214D - Neural Feud.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    switch(n){
+        case 1: cout<<"walk"<<endl; break;
+        case 2: cout<<"no"<<endl; break;
+        case 3: cout<<"no"<<endl; break;
+        case 4: cout<<"no"<<endl; break;
+        case 5: cout<<"yes"<<endl; break;
+        case 6: cout<<"yes"<<endl; break;
+        case 7: cout<<"backwards"<<endl; break;
+        case 8: cout<<"seven"<<endl; break;
+    }
+}
+
+int main()
+{
+    solve();
+        
+}
