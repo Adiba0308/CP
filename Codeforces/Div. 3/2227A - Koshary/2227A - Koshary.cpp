7cdce00b33618a4c120f0214b724f9b9f6193719
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-16 17:57:35 +0600

    [2227A - Koshary | 800] Accepted | Time: 31ms | Memory: 0KB

---
+++ Codeforces/Div. 3/2227A - Koshary/2227A - Koshary.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    long long x,y;
+    cin>>x>>y;
+    if(x%2==1 && y%2==1) cout<<"NO"<<endl;
+    else cout<<"YES"<<endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+        
+}
