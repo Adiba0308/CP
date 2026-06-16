88a68eece35174a543ec4bdc17200d5cad395873
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-16 17:57:21 +0600

    [2009A - Minimize! | 800] Accepted | Time: 15ms | Memory: 0KB

---
+++ Codeforces/Div. 4/2009A - Minimize!/2009A - Minimize!.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    long long a,b;
+    cin>>a>>b;
+    cout<<b-a<<endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+        
+}
