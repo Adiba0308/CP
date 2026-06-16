189b8d5c01e5f12c6f494af17cf19375ae4d2a2d
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-16 18:01:06 +0600

    [4A - Watermelon | 800] Accepted | Time: 92ms | Memory: 100KB

---
+++ Codeforces/Div. 2/4A - Watermelon/4A - Watermelon.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int a;
+    cin>>a;
+    if(a%2==0 && a>2)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+    
+}
+
+int main()
+{
+    int t;
+    solve();
+        
+}
