b3c41c74708ff5b27898ffd8be02a97091f12bb9
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-16 17:59:50 +0600

    [1873A - Short Sort | 800] Accepted | Time: 15ms | Memory: 100KB

---
+++ Codeforces/Div. 4/1873A - Short Sort/1873A - Short Sort.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin>>s;
+    if(s[0]!='a' && s[1]!='b' && s[2]!='c') cout<<"NO"<<endl;
+    else cout<<"YES"<<endl;
+ 
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+        
+}
