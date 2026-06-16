1a773c8dab8fe7d259da75e267291f8b59188cfc
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-16 17:57:25 +0600

    [2227C - Snowfall | 800] Accepted | Time: 296ms | Memory: 2000KB

---
+++ Codeforces/Div. 3/2227C - Snowfall/2227C - Snowfall.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    long long n;
+    cin>>n;
+    vector<long long>v(n),six,two,three,none;
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(v[i]%6==0)
+        {
+            six.push_back(v[i]);
+        }
+        else if(v[i]%2==0)
+        {
+            two.push_back(v[i]);
+        }
+        else if(v[i]%3==0)
+        {
+            three.push_back(v[i]);
+        }
+        else
+        {
+            none.push_back(v[i]);
+        }
+    }
+    for (int n : six) cout << n << " ";
+    for (int n : two) cout << n << " ";
+    for (int n : none) cout << n << " ";
+    for (int n : three) cout << n << " ";
+    
+    cout<<endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+        
+}
