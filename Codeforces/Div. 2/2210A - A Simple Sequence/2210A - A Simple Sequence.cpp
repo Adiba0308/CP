728750f222fbff68f4009bf1bf475df9100282ee
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-21 23:24:45 +0600

    [2210A - A Simple Sequence | 800] Accepted | Time: 31ms | Memory: 100KB

---
+++ Codeforces/Div. 2/2210A - A Simple Sequence/2210A - A Simple Sequence.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    for(int i=n;i>0;i--)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+        
+}
