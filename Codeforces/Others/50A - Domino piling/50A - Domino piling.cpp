36656ae8d6948f53f134d2a3aec7d4c5cf8a5bd6
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-21 23:17:45 +0600

    [50A - Domino piling | 800] Accepted | Time: 62ms | Memory: 100KB

---
+++ Codeforces/Others/50A - Domino piling/50A - Domino piling.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int m,n;
+    cin>>m>>n;
+    cout<<(m*n)/2<<endl;
+}
+
+int main()
+{
+    int t=1;
+    //cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+        
+}
