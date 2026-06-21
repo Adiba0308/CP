5c3c8748c6fe540c5960fb64997aedc5a433de56
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-21 23:19:27 +0600

    [118A - String Task | 1000] Accepted | Time: 92ms | Memory: 100KB

---
+++ Codeforces/Div. 2/118A - String Task/118A - String Task.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin>>s;
+    for(int i=0;i<s.length();i++)
+    {
+        s[i]=tolower(s[i]);
+        if(s[i]!='a' && s[i]!='e' && s[i]!='i' && s[i]!='o' && s[i]!='u' && s[i]!='y') 
+        {
+            cout<<"."<< s[i];
+        }
+    }
+}
+
+int main()
+{
+    solve();
+}
