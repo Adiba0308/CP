ca47321d8291ce5f8ace85af2c2edeb66f5d4371
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-21 23:17:29 +0600

    [1996A - Legs | 800] Accepted | Time: 31ms | Memory: 0KB

---
+++ Codeforces/Div. 3/1996A - Legs/1996A - Legs.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve()
+{
+  int n;
+  cin>>n;
+  cout<<(n/4)+((n%4)/2)<<endl;
+
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+        
+}
