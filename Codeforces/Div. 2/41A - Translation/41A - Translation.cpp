4020d23b60afbb000ab58c177bb79089ce44debb
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-21 23:17:51 +0600

    [41A - Translation | 800] Accepted | Time: 31ms | Memory: 100KB

---
+++ Codeforces/Div. 2/41A - Translation/41A - Translation.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    string a,b;
+    cin>>a>>b;
+    reverse(b.begin(), b.end());
+    if(a==b) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+    
+}
+
+int main()
+{
+    solve();
+        
+}
