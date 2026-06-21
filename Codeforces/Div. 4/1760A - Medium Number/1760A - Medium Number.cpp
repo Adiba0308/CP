32bcc234045b2505bdce1f91398766c1804f9645
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-21 23:21:28 +0600

    [1760A - Medium Number | 800] Accepted | Time: 46ms | Memory: 0KB

---
+++ Codeforces/Div. 4/1760A - Medium Number/1760A - Medium Number.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    vector < int > v = {a,b,c};
+    sort(v.begin(), v.end());
+    cout << v[1] << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
