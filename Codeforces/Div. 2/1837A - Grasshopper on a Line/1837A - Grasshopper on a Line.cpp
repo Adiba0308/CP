3ecb40925f7813850c8c368c8665158cc2a88bb4
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-21 23:21:48 +0600

    [1837A - Grasshopper on a Line | 800] Accepted | Time: 31ms | Memory: 0KB

---
+++ Codeforces/Div. 2/1837A - Grasshopper on a Line/1837A - Grasshopper on a Line.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+    int x, k;
+    cin >> x >> k ;
+    if(x%k!=0)
+    {
+        cout<<1<<"\n"<<x<<endl;
+        
+    }
+    else
+    {
+        cout<<2<<"\n"<<1<<" "<<x-1<<endl;
+        
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
