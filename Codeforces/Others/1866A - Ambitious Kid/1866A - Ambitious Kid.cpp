d4091832aa02572846a360d6ba2e35cd064afc3f
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-21 23:22:32 +0600

    [1866A - Ambitious Kid | 800] Accepted | Time: 140ms | Memory: 100KB

---
+++ Codeforces/Others/1866A - Ambitious Kid/1866A - Ambitious Kid.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	vector<int>v(n) ;
+	for(int i=0;i<n;i++)
+	{
+	    cin>>v[i];
+	    if(v[i]<0)
+	    {
+	        v[i]=v[i]*-1;
+	    }
+	}
+	cout<<*min_element(v.begin(), v.end());
+}
