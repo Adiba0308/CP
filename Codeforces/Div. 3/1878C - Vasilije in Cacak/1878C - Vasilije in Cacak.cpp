6c1a906f8adc45481606c2b8fc261dbb272028c3
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-21 23:20:00 +0600

    [1878C - Vasilije in Cacak | 900] Accepted | Time: 93ms | Memory: 0KB

---
+++ Codeforces/Div. 3/1878C - Vasilije in Cacak/1878C - Vasilije in Cacak.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+    long long n, k, x;
+    cin >> n >> k >> x;
+    long long min = k * (k + 1) / 2;
+    long long max = (n * (n + 1) / 2) - ((n - k) * (n - k + 1) / 2);
+    if (x >= min && x <= max) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+}
