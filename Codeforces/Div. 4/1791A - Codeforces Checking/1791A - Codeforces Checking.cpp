3c3bed4add086b64d2574e1049c592b5af358443
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-21 23:19:55 +0600

    [1791A - Codeforces Checking | 800] Accepted | Time: 31ms | Memory: 0KB

---
+++ Codeforces/Div. 4/1791A - Codeforces Checking/1791A - Codeforces Checking.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+   string s;
+   cin>>s;
+   string c="codeforces";
+   if(c.find(s) != string::npos) cout<<"YES"<<endl;
+   else cout<<"NO"<<endl;
+   
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+        
+}
