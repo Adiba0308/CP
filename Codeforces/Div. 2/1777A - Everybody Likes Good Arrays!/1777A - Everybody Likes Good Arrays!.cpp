723e77aae66f12c069d9dcf24efe7d995909f1b4
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-21 23:20:57 +0600

    [1777A - Everybody Likes Good Arrays! | 800] Accepted | Time: 109ms | Memory: 100KB

---
+++ Codeforces/Div. 2/1777A - Everybody Likes Good Arrays!/1777A - Everybody Likes Good Arrays!.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n,count=0;
+    cin>>n;
+    int a[n];
+    
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    
+    for(int i=0;i<n-1;i++)
+    {
+        if(a[i]%2)
+        {
+            if(a[i+1]%2)
+            {
+                count++;
+            }
+        }
+        else
+        {
+            if(a[i+1]%2==0)
+            {
+                count++;
+            }
+        }
+    }
+    cout<<count<<endl;
+  
+    
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+        
+}
