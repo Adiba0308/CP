f0c1c46ca4a8b9f3149da4b96f4f27311d3075c3
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-06-26 22:28:09 +0600

    [1433A - Boring Apartments | 800] Accepted | Time: 46ms | Memory: 0KB

---
+++ Codeforces/Div. 3/1433A - Boring Apartments/1433A - Boring Apartments.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve()
+{
+    int n;
+    cin>>n;
+    int r,c=0,count=0;
+    while(n)
+    {
+        r=n%10;
+        c++;
+        count+=c;
+        n=n/10;
+    }
+    cout<<count+((r-1)*(1+2+3+4))<<endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+        
+}
