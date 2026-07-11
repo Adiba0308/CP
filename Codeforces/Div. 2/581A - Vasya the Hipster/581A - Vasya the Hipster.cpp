c7a41d75f90f7ecdcd512e9ad64c57987d8779e4
Adiba0308 <Adiba0308@users.noreply.github.com>
2026-07-11 23:15:13 +0600

    [581A - Vasya the Hipster | 800] Accepted | Time: 46ms | Memory: 0KB

--- Codeforces/Div. 2/581A - Vasya the Hipster/581A - Vasya the Hipster.cpp
+++ Codeforces/Div. 2/581A - Vasya the Hipster/581A - Vasya the Hipster.cpp
@@ -1,16 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define int long long
+
 void solve()
 {
     int a,b;
     cin>>a>>b;
-    int max=std::max(a,b);
-    int min=std::min(a,b);
-    cout<<min<<" "<<(max-min)/2;
+    cout<<min(a,b)<<" "<<abs(a-b)/2<<endl;
 }
 
-int main()
-{
-    solve();
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t=1;
+    //cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+        
 }
